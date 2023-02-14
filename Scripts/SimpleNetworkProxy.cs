543904c4a3b0d2493f35abfe8eb3cc47b6878353
tutinoco <m@tutino.co>
2023-02-14 18:12:09 +0900

    SimpleNetworkUdonBehaviourの仕組みを根本から見直して大幅に修正
    SimpleNetworkUdonBehaviourからSimpleNetworkにツールの名前を変更しました。
    SimpleNetworkクラスを新しく作成し、Scene内にPrefabを配置して利用する仕様に変更しました。
    SimpleNetworkProxyクラスを作成し、オブジェクト所有権に依存しない通信を実現しました。
    また、この通信を実現するにあたり、全てのSimpleNetworkBehaviourをSimpleNetworkで管理させ、内部的に受信するターゲットを指定できる仕様に変更しました。
    SimpleNetworkUdonBehaviourクラスは、SimpleNetworkBehaviourに名前変更し、ほとんどの機能をSimpleNetworkクラスが担うようにしました。
    コマンドの区切り文字に使用していた半角中黒（･）文字の利用をやめ、Group Separator文字とRecord Separator文字の利用に変更しました。
    コマンドIDやターゲットIDに16進数を利用してコマンド文字数を削減しました。

---
+++ Scripts/SimpleNetworkProxy.cs
@@ -0,0 +1,30 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tutinoco
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class SimpleNetworkProxy : UdonSharpBehaviour
+    {
+        public SimpleNetwork sn;
+
+        [UdonSynced(UdonSyncMode.None), FieldChangeCallback(nameof(cmd))] private string _cmd;
+        public string cmd {
+            get { return _cmd; }
+            set { sn.ReceiveCommand(_cmd=value, Networking.GetOwner(gameObject)); }
+        }
+
+        public void SendCommand(string command)
+        {
+            cmd = command;
+            RequestSerialization();
+        }
+
+        public override void OnOwnershipTransferred(VRCPlayerApi player)
+        {
+            sn.OnProxyOwnershipTransferred(this);
+        }
+    }
+}
