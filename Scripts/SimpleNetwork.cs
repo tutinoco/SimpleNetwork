8265b5ed7f6bbfac28fb14a5513a4bc8dd6c3558
tutinoco <m@tutino.co>
2023-02-14 18:12:09 +0900

    SimpleNetworkUdonBehaviourの仕組みを根本から見直して大幅に修正
    SimpleNetworkUdonBehaviourからSimpleNetworkにモジュールの名前を変更しました。
    SimpleNetworkクラスを新しく作成し、Scene内にPrefabを配置して利用する仕様に変更しました。
    SimpleNetworkProxyクラスを作成し、オブジェクト所有権に依存しない通信を実現しました。
    また、この通信を実現するにあたり、全てのSimpleNetworkBehaviourをSimpleNetworkで管理させ、内部的に受信するターゲットを指定できる仕様に変更しました。
    SimpleNetworkUdonBehaviourクラスは、SimpleNetworkBehaviourに名前変更し、ほとんどの機能をSimpleNetworkクラスが担うようにしました。
    コマンドの区切り文字に使用していた半角中黒（･）文字の利用をやめ、Group Separator文字とRecord Separator文字の利用に変更しました。
    コマンドIDやターゲットIDに16進数を利用してコマンド文字数を削減しました。

---
+++ Scripts/SimpleNetwork.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tutinoco
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual), DefaultExecutionOrder(-1024)]
+    public class SimpleNetwork : UdonSharpBehaviour
+    {
+        private SimpleNetworkBehaviour[] behaviours = new SimpleNetworkBehaviour[0];
+        private SimpleNetworkProxy[] proxys;
+        [UdonSynced(UdonSyncMode.None)] private int[] refer;
+        private string command;
+        private char gs = (char)0x1D;
+        private char rs = (char)0x1E;
+        private bool isReady;
+
+        public static SimpleNetwork GetInstance()
+        {
+            GameObject g = GameObject.Find("SimpleNetwork");
+            return g.GetComponent<SimpleNetwork>();
+        }
+
+        void Start()
+        {
+            command = GenerateCmdId();
+
+            int len = gameObject.transform.childCount;
+            proxys = new SimpleNetworkProxy[len+1];
+            refer = new int[len+1];
+            for(int i=1; i<len; i++) {
+                Transform t = transform.GetChild(i);
+                proxys[i] = t.gameObject.GetComponent<SimpleNetworkProxy>();
+                proxys[i].sn = this;
+            }
+        }
+
+        void Update()
+        {
+            if( isReady && command.Length!=4 ) {
+                int pid = Networking.LocalPlayer.playerId;
+                SimpleNetworkProxy p = proxys[refer[pid]];
+                p.SendCommand(command);
+                command = GenerateCmdId();
+            } 
+        }
+
+        public void ReceiveCommand( string cmd, VRCPlayerApi player )
+        {
+            foreach( string record in cmd.Substring(4).Split(gs) ) {
+                string[] data = record.Split(rs);
+                uint i = UInt32.Parse(data[0], System.Globalization.NumberStyles.HexNumber);
+                behaviours[i].ReceiveEvent(data[1], data[2], player);
+            }
+        }
+
+        private string GenerateCmdId() { return string.Format("{0:X4}", UnityEngine.Random.Range(0, 65536)); }
+
+        public void Register(SimpleNetworkBehaviour behaviour)
+        {
+            int index = behaviours.Length;
+            string x = Networking.GetUniqueName(behaviour.gameObject);
+            for (int i = 0; i < behaviours.Length; i++) {
+                string y = Networking.GetUniqueName(behaviours[i].gameObject);
+                if (y.CompareTo(x) > 0) { index=i; break; }
+            }
+            var temp = new SimpleNetworkBehaviour[behaviours.Length + 1];
+            for (int i = 0; i < index; i++) temp[i] = behaviours[i];
+            behaviour._id = (uint)index;
+            temp[index] = behaviour;
+            for (int i = index+1; i<temp.Length; i++) { behaviours[i-1]._id++; temp[i]=behaviours[i-1]; }
+            behaviours = temp;
+        }
+
+        public void AddRecord( string record )
+        {
+            command = command+(command.Length==4?"":gs.ToString())+record;
+        }
+
+        public override void OnPlayerJoined(VRCPlayerApi player)
+        {
+            if( !Networking.IsMaster ) return;
+
+            int pid = player.playerId;
+            for(var i=0; i<proxys.Length; i++) {
+                SimpleNetworkProxy proxy = proxys[i];
+                if( IsProxyUsed(i) ) continue;
+                refer[pid] = i;
+                if( Networking.IsOwner(proxy.gameObject) ) OnProxyOwnershipTransferred(proxy);
+                else Networking.SetOwner(player, proxy.gameObject);
+                Debug.Log("新しいユーザに"+i+"番のProxyを割り当てました");
+                break;
+            }
+
+            RequestSerialization();
+        }
+
+        private bool IsProxyUsed( int i )
+        {
+            foreach(int j in refer) if(j == i) return true;
+            return false;
+        }
+
+        public void OnProxyOwnershipTransferred( SimpleNetworkProxy proxy )
+        {
+            if( Networking.LocalPlayer == Networking.GetOwner(proxy.gameObject) ) isReady = true;
+        }
+        
+        public override void OnPlayerLeft(VRCPlayerApi player)
+        {
+            int pid = player.playerId;
+            Debug.Log(refer[pid]+"番のProxyを解除しました");
+            refer[pid] = 0;
+        }
+    }
+}
---
+++ Scripts/SimpleNetworkBehaviour.cs
@@ -0,0 +1,30 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace tutinoco
+{
+    public class SimpleNetworkBehaviour : UdonSharpBehaviour
+    {
+        public uint _id;
+        private SimpleNetwork _sn;
+        private char _rs = (char)0x1E;
+
+        protected void Initialize()
+        {
+            _sn = SimpleNetwork.GetInstance();
+            _sn.Register(this);
+        }
+
+        public void SendEvent(string name, string value)
+        {
+            string record = _id.ToString("X")+_rs+name+_rs+value;
+            _sn.AddRecord(record);
+        }
+
+        public virtual void ReceiveEvent(string name, string value, VRCPlayerApi player) { }
+    }
+}
+
