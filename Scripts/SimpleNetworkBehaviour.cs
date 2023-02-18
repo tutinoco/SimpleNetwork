50ab2b15d2b08d9a96198fc14118376b869f9254
tutinoco <m@tutino.co>
2023-02-18 10:56:00 +0900

    SimpleNetworkBehaviorで行っていたイベント遅延実行処理をSimpleNetworkに移し不要なメソッドを削除しました。
    SimpleNetworkのcommandプロパティを廃止し、Update時に毎回生成するようにしました。
    SendRecord()メソッドをSetEvent()メソッドに変更し、イベントをcommand文字列に蓄える方法からrIsSyncs, rDelays, rSources, rNames, rValues配列に蓄えるようにしました。
    また、rSourcesによりBehaviorを参照できるようになったため、全てのBehaviorの登録が完了する前にSetEventが実行されることでIDがズレる不具合も修正しました。
    またこの変更に伴って不要になったIsReadyメソッドを廃止しました。
    新しく追加したr系の配列に要素を追加するメソッドInsertElement()を作成しました。
    ExecRecord()メソッド内でやることがほとんどなくなったため、廃止して呼んでいた箇所に直接書きました。
    SimpleNetworkBehaviorの処理をSimpleNetworkに移したため、Behaviorから利用していたCreateRecord()メソッドを廃止しました。
    また、SimpleNetworkBehaviorの_Update()も廃止され、遅延実行処理メソッドであるSetRecordDelayed()もSimpleNetworkのrDelays配列で管理されるようになりました。
    また、それに伴ってSimpleNetworkBehaviorで不要になったプロパティrs, delayDatas, delayCountsを削除しました。

--- Scripts/SimpleNetworkBehaviour.cs
+++ Scripts/SimpleNetworkBehaviour.cs
@@ -1,5 +1,4 @@
-﻿
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
@@ -9,89 +8,51 @@ namespace tutinoco
     public class SimpleNetworkBehaviour : UdonSharpBehaviour
     {
         public uint _id;
-
         private SimpleNetwork sn;
-        private char rs = (char)0x1E;
-
-        private string[] delayDatas = new string[0];
-        private int[] delayCounts = new int[0];
 
         public void SimpleNetworkInit()
         {
+            if( sn != null ) return;
             sn = SimpleNetwork.GetInstance();
             sn.Register(this);
         }
 
-        private void SetRecordDelayed(int delay, bool isSync, string name, string value, string target)
-        {
-            for (int i = 0; i < delayCounts.Length; i++) {
-                if (delayCounts[i] >= 0) continue;
-                delayCounts[i] = delay;
-                delayDatas[i] = (isSync?"1":"0")+rs+name+rs+value+rs+target;
-                return;
-            }
-
-            int[] temp = new int[delayCounts.Length+1];
-            delayCounts.CopyTo(temp, 0);
-            temp[temp.Length-1] = delay;
-            delayCounts = temp;
-
-            string[] temp2 = new string[delayDatas.Length+1];
-            delayDatas.CopyTo(temp2, 0);
-            temp2[temp2.Length-1] = (isSync?"1":"0")+rs+name+rs+value+rs+target;
-            delayDatas = temp2;
-        }
-
-        private void SetRecord(bool isSync, string name, string value, string target)
-        {
-            if( sn==null || !sn.IsReady() ) { SetRecordDelayed(0, isSync, name, value, target); return; }
-            if( target == "" ) target = _id.ToString("X");
-            string record = sn.CreateRecord(name, value, target);
-            if( isSync ) sn.SendRecord(record);
-            else sn.ExecRecord(record);
-        }
-
-        public void _Update()
-        {
-            for (int i = 0; i < delayCounts.Length; i++) {
-                if( delayCounts[i] == 0 ) {
-                    string[] d = delayDatas[i].Split(rs);
-                    SetRecord(d[0]!="0", d[1], d[2], d[3]);
-                }
-                if( delayCounts[i] >= 0 ) delayCounts[i]--;
-            }
-        }
-
         public virtual void OnSimpleNetworkReady() { }
+
         public virtual void ReceiveEvent(string name, string value) { }
 
+        private void SetEvent(bool isSync, string name, string value, int delay )
+        {
+            if( sn == null ) SimpleNetworkInit();
+            sn.SetEvent(isSync, delay, this, name, value);
+        }
+
         // string
-        public void ExecEvent(string name, string value="", string target="") { SetRecord(false, name, value, target); }
-        public void SendEvent(string name, string value="", string target="") { SetRecord(true, name, value, target); }
+        public void ExecEvent(string name, string value="", int delay=0) { SetEvent(false, name, value, delay); }
+        public void SendEvent(string name, string value="", int delay=0) { SetEvent(true, name, value, delay); }
 
         // bool
-        public void ExecEvent(string name, bool value, string target="") { SetRecord(false, name, _Bool(value), target); }
-        public void SendEvent(string name, bool value, string target="") { SetRecord(true, name, _Bool(value), target); }
+        public void ExecEvent(string name, bool value, int delay=0) { SetEvent(false, name, _Bool(value), delay); }
+        public void SendEvent(string name, bool value, int delay=0) { SetEvent(true, name, _Bool(value), delay); }
         public static bool GetBool( string v ) { return v!="0"; }
         private string _Bool( bool v ) { return v?"1":"0"; }
 
         // int        
-        public void ExecEvent(string name, int value, string target="") { SetRecord(false, name, _Int(value), target); }
-        public void SendEvent(string name, int value, string target="") { SetRecord(true, name, _Int(value), target); }
+        public void ExecEvent(string name, int value, int delay=0) { SetEvent(false, name, _Int(value), delay); }
+        public void SendEvent(string name, int value, int delay=0) { SetEvent(true, name, _Int(value), delay); }
         public static int GetInt( string v ) { return int.Parse(v); }
         private string _Int( int v ) { return ""+v; }
 
         // float
-        public void ExecEvent(string name, float value, string target="") { SetRecord(false, name, _Float(value), target); }
-        public void SendEvent(string name, float value, string target="") { SetRecord(true, name, _Float(value), target); }
+        public void ExecEvent(string name, float value, int delay=0) { SetEvent(false, name, _Float(value), delay); }
+        public void SendEvent(string name, float value, int delay=0) { SetEvent(true, name, _Float(value), delay); }
         public static float GetFloat( string v ) { return float.Parse(v); }
         private string _Float( float v ) { return ""+v; }
 
         // Vector3
-        public void ExecEvent(string name, Vector3 value, string target="") { SetRecord(false, name, _Vector3(value), target); }
-        public void SendEvent(string name, Vector3 value, string target="") { SetRecord(true, name, _Vector3(value), target); }
+        public void ExecEvent(string name, Vector3 value, int delay=0) { SetEvent(false, name, _Vector3(value), delay); }
+        public void SendEvent(string name, Vector3 value, int delay=0) { SetEvent(true, name, _Vector3(value), delay); }
         public static Vector3 GetVector3( string v ) { string[] d=v.Split(','); return new Vector3(float.Parse(d[0]), float.Parse(d[1]), float.Parse(d[2])); }
         private string _Vector3( Vector3 v ) { return v.x+","+v.y+","+v.z; }
     }
 }
-
