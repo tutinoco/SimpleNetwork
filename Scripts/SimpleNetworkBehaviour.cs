35d53fbe200ed64f3738ef7d270ad70617c55aa4
tutinoco <m@tutino.co>
2023-02-18 21:38:57 +0900

    イベントの送信先を設定できるように改善
    SimpleNetwork.csに送信先の条件が定義されたEnum、SimpleNetworkTargetを新しく作成しました。
    rIsSyncsが廃止され代わりにrTargetsに置き換わり、同期する必要のないイベントはSimpleNetworkTarget.Selfで設定するようにしました。
    SimpleNetworkのSetEvent()メソッドで、intでもenumでもTargetを指定できるようにするため、オーバーロードメソッドを作成しました。
    また、SimpleNetworkBehaviorでもSetEvent()メソッドのオーバーロードメソッドを作成しています。
    SimpleNetwork.csのOnProxySynced()メソッド内にユーザがTargetに該当しないければイベントを無視する仕組みを導入しました。
    SimpleNetworkBehaviorで、Targetを指定したメソッドの呼び方ができるようにそれぞれのメソッドのオーバーロードを作成しました。

--- Scripts/SimpleNetworkBehaviour.cs
+++ Scripts/SimpleNetworkBehaviour.cs
@@ -21,37 +21,53 @@ namespace tutinoco
 
         public virtual void ReceiveEvent(string name, string value) { }
 
-        private void SetEvent(bool isSync, string name, string value, int delay )
+        private void SetEvent(SimpleNetworkTarget target, string name, string value, int delay )
         {
             if( sn == null ) SimpleNetworkInit();
-            sn.SetEvent(isSync, delay, this, name, value);
+            sn.SetEvent(this, target, name, value, delay);
+        }
+
+        private void SetEvent(int target, string name, string value, int delay )
+        {
+            if( sn == null ) SimpleNetworkInit();
+            sn.SetEvent(this, target, name, value, delay);
         }
 
         // string
-        public void ExecEvent(string name, string value="", int delay=0) { SetEvent(false, name, value, delay); }
-        public void SendEvent(string name, string value="", int delay=0) { SetEvent(true, name, value, delay); }
+        public void ExecEvent(string name, string value="", int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, value, delay); }
+        public void SendEvent(string name, string value="", int delay=0) { SetEvent(SimpleNetworkTarget.All, name, value, delay); }
+        public void SendEvent(SimpleNetworkTarget target, string name, string value="", int delay=0) { SetEvent(target, name, value, delay); }
+        public void SendEvent(int target, string name, string value="", int delay=0) { SetEvent(target, name, value, delay); }
 
         // bool
-        public void ExecEvent(string name, bool value, int delay=0) { SetEvent(false, name, _Bool(value), delay); }
-        public void SendEvent(string name, bool value, int delay=0) { SetEvent(true, name, _Bool(value), delay); }
+        public void ExecEvent(string name, bool value, int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, _Bool(value), delay); }
+        public void SendEvent(string name, bool value, int delay=0) { SetEvent(SimpleNetworkTarget.All, name, _Bool(value), delay); }
+        public void SendEvent(SimpleNetworkTarget target, string name, bool value, int delay=0) { SetEvent(target, name, _Bool(value), delay); }
+        public void SendEvent(int target, string name, bool value, int delay=0) { SetEvent(target, name, _Bool(value), delay); }
         public static bool GetBool( string v ) { return v!="0"; }
         private string _Bool( bool v ) { return v?"1":"0"; }
 
-        // int        
-        public void ExecEvent(string name, int value, int delay=0) { SetEvent(false, name, _Int(value), delay); }
-        public void SendEvent(string name, int value, int delay=0) { SetEvent(true, name, _Int(value), delay); }
+        // int
+        public void ExecEvent(string name, int value, int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, _Int(value), delay); }
+        public void SendEvent(string name, int value, int delay=0) { SetEvent(SimpleNetworkTarget.All, name, _Int(value), delay); }
+        public void SendEvent(SimpleNetworkTarget target, string name, int value, int delay=0) { SetEvent(target, name, _Int(value), delay); }
+        public void SendEvent(int target, string name, int value, int delay=0) { SetEvent(target, name, _Int(value), delay); }
         public static int GetInt( string v ) { return int.Parse(v); }
         private string _Int( int v ) { return ""+v; }
 
         // float
-        public void ExecEvent(string name, float value, int delay=0) { SetEvent(false, name, _Float(value), delay); }
-        public void SendEvent(string name, float value, int delay=0) { SetEvent(true, name, _Float(value), delay); }
+        public void ExecEvent(string name, float value, int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, _Float(value), delay); }
+        public void SendEvent(string name, float value, int delay=0) { SetEvent(SimpleNetworkTarget.All, name, _Float(value), delay); }
+        public void SendEvent(SimpleNetworkTarget target, string name, float value, int delay=0) { SetEvent(target, name, _Float(value), delay); }
+        public void SendEvent(int target, string name, float value, int delay=0) { SetEvent(target, name, _Float(value), delay); }
         public static float GetFloat( string v ) { return float.Parse(v); }
         private string _Float( float v ) { return ""+v; }
 
         // Vector3
-        public void ExecEvent(string name, Vector3 value, int delay=0) { SetEvent(false, name, _Vector3(value), delay); }
-        public void SendEvent(string name, Vector3 value, int delay=0) { SetEvent(true, name, _Vector3(value), delay); }
+        public void ExecEvent(string name, Vector3 value, int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, _Vector3(value), delay); }
+        public void SendEvent(string name, Vector3 value, int delay=0) { SetEvent(SimpleNetworkTarget.All, name, _Vector3(value), delay); }
+        public void SendEvent(SimpleNetworkTarget target, string name, Vector3 value, int delay=0) { SetEvent(target, name, _Vector3(value), delay); }
+        public void SendEvent(int target, string name, Vector3 value, int delay=0) { SetEvent(target, name, _Vector3(value), delay); }
         public static Vector3 GetVector3( string v ) { string[] d=v.Split(','); return new Vector3(float.Parse(d[0]), float.Parse(d[1]), float.Parse(d[2])); }
         private string _Vector3( Vector3 v ) { return v.x+","+v.y+","+v.z; }
     }
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 ﻿# SimpleNetwork
-![SimpleNetwork2](https://user-images.githubusercontent.com/14051445/219826822-95ede90d-f5c7-4679-9218-8e9c4c598dc2.png)
+![SimpleNetworkMonster](https://user-images.githubusercontent.com/14051445/219826822-95ede90d-f5c7-4679-9218-8e9c4c598dc2.png)
 SimpleNetworkは、VRChatのNetworkingがしんどい方のためのNetworkingラッパーなUdonSharpモジュールです。
 SendCustomNetworkEventメソッドで、引数を扱えない問題を解消する目的で作成されました。
 
@@ -8,8 +8,46 @@ SendCustomNetworkEventメソッドで、引数を扱えない問題を解消す�
 * オブジェクトの所有権にかかわらず低レイテンシーな高速通信ができます。
 * `SendCustomNetworkEventDelayedFrames`に相当するネットワークイベントの遅延実行ができます。
 * 連続でイベントを叩いても自動的に1回の通信にまとめるから安定して動作します。
-* `All` `Owner`の他に`Master`や`playerId`を指定して特定のユーザにイベントを送信できます。（実装中）
+* `All` `Owner`の他に`Master`や`playerId`を指定して特定のユーザにイベントを送信できます。
 * `Master`にイベント送信を依頼するなど、ユーザを指定してイベントの送信を仲介させることができます。（実装中）
 * オブジェクトにタグを付けることで別のオブジェクトにイベントを送信することができます。（実装中）
 * 最後に実行したイベントやイベントログをサーバに保存し、Joinした人に同期できます。（実装中）
 * シーンに存在しない複製したオブジェクトも同期できます。（実装中）
+
+## 使い方
+
+現在この項目は書き途中です。
+
+### 送信先の指定
+```
+// 全員にイベントを送信
+SendEvent(SimpleNetworkTarget.All, "Jump", 2.0f);
+
+// オブジェクト所有者にイベントを送信
+SendEvent(SimpleNetworkTarget.Owner, "Jump", 2.0f);
+
+// インスタンスマスターにイベントを送信
+SendEvent(SimpleNetworkTarget.Master, "Jump", 2.0f);
+
+// 自分自身にイベントを送信（ExecEventに同じ）
+SendEvent(SimpleNetworkTarget.Self, "Jump", 2.0f);
+
+// オブジェクト所有者以外にイベントを送信
+SendEvent(SimpleNetworkTarget.NotOwner, "Jump", 2.0f);
+
+// インスタンスマスター以外にイベントを送信
+SendEvent(SimpleNetworkTarget.NotMaster, "Jump", 2.0f);
+
+// 自分以外にイベントを送信
+SendEvent(SimpleNetworkTarget.NotSelf, "Jump", 2.0f);
+
+// プレイヤーIDが7のユーザにイベントを送信
+SendEvent(7, "Jump", 2.0f);
+
+// プレイヤーIDが7以外のユーザにイベントを送信
+SendEvent(~7, "Jump", 2.0f);
+```
+
+### コマンド
+SimpleNetworkが通信するコマンドは以下の通りです。
+![Command](https://user-images.githubusercontent.com/14051445/219865470-36cc6020-74e4-4560-999e-d69e33780cb4.png)
