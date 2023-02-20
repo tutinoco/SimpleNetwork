b1cf95621cd59ae8ca250f58a3b002ade6ae3af6
tutinoco <m@tutino.co>
2023-02-21 00:17:01 +0900

    commandと呼んでいた通信文字列の名称をcodeに変更し、codeに使われている数値を94進数の固定長にしてcodeの圧縮率を高めました。
    94進数変換するメソッドが定義されたSimpleNetworkConverterクラスを新しく作成しました。
    Enum型のSimpleNetworkTargetの名称をSendToに変更しました。
    また、rTarget[]配列の名称もrSendTo[]に変更しました。
    SimpleNetworkにデバッグモードを搭載しました。
    コードの先頭に割り当てられていた4文字のランダムの16進数を廃止し、新しくsynccharと名付けた1文字のインクリメントされる94進数に変更しました。
    それに伴ってGenerateCmdId()メソッドを廃止しました。
    SimpleNetworkが管理できるオブジェクト数を変更する定数behaviorDigitを定義しました。
    コードに含まれるSourceやSendToなどの情報が固定長になったことで特殊区切り文字の使用を削減した影響でレコードのデータを分割する際にSplit()メソッドでは単純に分割できなくなったため、新しくSplitRecord()メソッドとついでにJoinRecord()メソッドも作成しました。
    送受信可能なデータにObjectを追加しIDからオブジェクトを取得するためのGetBehaviour()メソッドを作成しました。
    それに伴い、Register()メソッドの名前をSetBehaviour()に変更しました。
    publicなSetEvent()メソッドでSendToの値をintで取得していたのをやめ、VRCPlayerApiで取って内部的にplayerIDを取得するようにしました。

--- Scripts/SimpleNetworkBehaviour.cs
+++ Scripts/SimpleNetworkBehaviour.cs
@@ -14,61 +14,69 @@ namespace tutinoco
         {
             if( sn != null ) return;
             sn = SimpleNetwork.GetInstance();
-            sn.Register(this);
+            sn.SetBehaviour(this);
         }
 
         public virtual void OnSimpleNetworkReady() { }
 
         public virtual void ReceiveEvent(string name, string value) { }
 
-        private void SetEvent(SimpleNetworkTarget target, string name, string value, int delay )
+        private void SetEvent(SendTo sendto, string name, string value, int delay )
         {
             if( sn == null ) SimpleNetworkInit();
-            sn.SetEvent(this, target, name, value, delay);
+            sn.SetEvent(this, sendto, name, value, delay);
         }
 
-        private void SetEvent(int target, string name, string value, int delay )
+        private void SetEvent(VRCPlayerApi sendto, string name, string value, int delay )
         {
             if( sn == null ) SimpleNetworkInit();
-            sn.SetEvent(this, target, name, value, delay);
+            sn.SetEvent(this, sendto, name, value, delay);
         }
 
         // string
-        public void ExecEvent(string name, string value="", int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, value, delay); }
-        public void SendEvent(string name, string value="", int delay=0) { SetEvent(SimpleNetworkTarget.All, name, value, delay); }
-        public void SendEvent(SimpleNetworkTarget target, string name, string value="", int delay=0) { SetEvent(target, name, value, delay); }
-        public void SendEvent(int target, string name, string value="", int delay=0) { SetEvent(target, name, value, delay); }
+        public void ExecEvent(string name, string value="", int delay=0) { SetEvent(SendTo.Self, name, value, delay); }
+        public void SendEvent(string name, string value="", int delay=0) { SetEvent(SendTo.All, name, value, delay); }
+        public void SendEvent(SendTo sendto, string name, string value="", int delay=0) { SetEvent(sendto, name, value, delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, string value="", int delay=0) { SetEvent(sendto, name, value, delay); }
 
         // bool
-        public void ExecEvent(string name, bool value, int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, _Bool(value), delay); }
-        public void SendEvent(string name, bool value, int delay=0) { SetEvent(SimpleNetworkTarget.All, name, _Bool(value), delay); }
-        public void SendEvent(SimpleNetworkTarget target, string name, bool value, int delay=0) { SetEvent(target, name, _Bool(value), delay); }
-        public void SendEvent(int target, string name, bool value, int delay=0) { SetEvent(target, name, _Bool(value), delay); }
-        public static bool GetBool( string v ) { return v!="0"; }
+        public void ExecEvent(string name, bool value, int delay=0) { SetEvent(SendTo.Self, name, _Bool(value), delay); }
+        public void SendEvent(string name, bool value, int delay=0) { SetEvent(SendTo.All, name, _Bool(value), delay); }
+        public void SendEvent(SendTo sendto, string name, bool value, int delay=0) { SetEvent(sendto, name, _Bool(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, bool value, int delay=0) { SetEvent(sendto, name, _Bool(value), delay); }
+        public bool GetBool( string v ) { return v!="0"; }
         private string _Bool( bool v ) { return v?"1":"0"; }
 
         // int
-        public void ExecEvent(string name, int value, int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, _Int(value), delay); }
-        public void SendEvent(string name, int value, int delay=0) { SetEvent(SimpleNetworkTarget.All, name, _Int(value), delay); }
-        public void SendEvent(SimpleNetworkTarget target, string name, int value, int delay=0) { SetEvent(target, name, _Int(value), delay); }
-        public void SendEvent(int target, string name, int value, int delay=0) { SetEvent(target, name, _Int(value), delay); }
-        public static int GetInt( string v ) { return int.Parse(v); }
+        public void ExecEvent(string name, int value, int delay=0) { SetEvent(SendTo.Self, name, _Int(value), delay); }
+        public void SendEvent(string name, int value, int delay=0) { SetEvent(SendTo.All, name, _Int(value), delay); }
+        public void SendEvent(SendTo sendto, string name, int value, int delay=0) { SetEvent(sendto, name, _Int(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, int value, int delay=0) { SetEvent(sendto, name, _Int(value), delay); }
+        public int GetInt( string v ) { return int.Parse(v); }
         private string _Int( int v ) { return ""+v; }
 
         // float
-        public void ExecEvent(string name, float value, int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, _Float(value), delay); }
-        public void SendEvent(string name, float value, int delay=0) { SetEvent(SimpleNetworkTarget.All, name, _Float(value), delay); }
-        public void SendEvent(SimpleNetworkTarget target, string name, float value, int delay=0) { SetEvent(target, name, _Float(value), delay); }
-        public void SendEvent(int target, string name, float value, int delay=0) { SetEvent(target, name, _Float(value), delay); }
-        public static float GetFloat( string v ) { return float.Parse(v); }
+        public void ExecEvent(string name, float value, int delay=0) { SetEvent(SendTo.Self, name, _Float(value), delay); }
+        public void SendEvent(string name, float value, int delay=0) { SetEvent(SendTo.All, name, _Float(value), delay); }
+        public void SendEvent(SendTo sendto, string name, float value, int delay=0) { SetEvent(sendto, name, _Float(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, float value, int delay=0) { SetEvent(sendto, name, _Float(value), delay); }
+        public float GetFloat( string v ) { return float.Parse(v); }
         private string _Float( float v ) { return ""+v; }
 
         // Vector3
-        public void ExecEvent(string name, Vector3 value, int delay=0) { SetEvent(SimpleNetworkTarget.Self, name, _Vector3(value), delay); }
-        public void SendEvent(string name, Vector3 value, int delay=0) { SetEvent(SimpleNetworkTarget.All, name, _Vector3(value), delay); }
-        public void SendEvent(SimpleNetworkTarget target, string name, Vector3 value, int delay=0) { SetEvent(target, name, _Vector3(value), delay); }
-        public void SendEvent(int target, string name, Vector3 value, int delay=0) { SetEvent(target, name, _Vector3(value), delay); }
-        public static Vector3 GetVector3( string v ) { string[] d=v.Split(','); return new Vector3(float.Parse(d[0]), float.Parse(d[1]), float.Parse(d[2])); }
+        public void ExecEvent(string name, Vector3 value, int delay=0) { SetEvent(SendTo.Self, name, _Vector3(value), delay); }
+        public void SendEvent(string name, Vector3 value, int delay=0) { SetEvent(SendTo.All, name, _Vector3(value), delay); }
+        public void SendEvent(SendTo sendto, string name, Vector3 value, int delay=0) { SetEvent(sendto, name, _Vector3(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, Vector3 value, int delay=0) { SetEvent(sendto, name, _Vector3(value), delay); }
+        public Vector3 GetVector3( string v ) { string[] d=v.Split(','); return new Vector3(float.Parse(d[0]), float.Parse(d[1]), float.Parse(d[2])); }
         private string _Vector3( Vector3 v ) { return v.x+","+v.y+","+v.z; }
+
+        // Object
+        public void ExecEvent(string name, SimpleNetworkBehaviour value, int delay=0) { SetEvent(SendTo.Self, name, _Object(value), delay); }
+        public void SendEvent(string name, SimpleNetworkBehaviour value, int delay=0) { SetEvent(SendTo.All, name, _Object(value), delay); }
+        public void SendEvent(SendTo sendto, string name, SimpleNetworkBehaviour value, int delay=0) { SetEvent(sendto, name, _Object(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, SimpleNetworkBehaviour value, int delay=0) { SetEvent(sendto, name, _Object(value), delay); }
+        public SimpleNetworkBehaviour GetObject( string v ) { return sn.GetBehaviour(int.Parse(v)); }
+        private string _Object( SimpleNetworkBehaviour v ) { return ""+v._id; }
     }
 }
--- readme.md
+++ readme.md
@@ -8,9 +8,9 @@ SimpleNetworkは、[SimpleNetworkUdonBehavior](https://github.com/tutinoco/Simpl
 ## 特徴
 * `SendCustomNetworkEvent`で不可能な引数の送信ができ、メッセージングプログラミングを可能にします。
 * 煩わしい所有権から解放され、権限の無いオブジェクトからも低レイテンシーで高速な通信が行えます。
-* 異なるオブジェクトにイベントを送信できる他、グループに設定して複数のオブジェクトにイベントを送信できます。◆
+* オブジェクトにグループを設定して、複数のオブジェクトにイベントをイテレーション送信できます。◆
 * 大量にイベントを発行しても、自動的に全てのイベントを1回の送信にまとめるため、安定して動作します。
-* `All`や`Owner`のほかに、`Master`や`playerId`を設定して、イベントの受信者を限定することができます。
+* `All`や`Owner`のほかに、`Master`や`player`を設定して、イベントの受信者を限定することができます。
 * `Master`にイベント送信を依頼するなど、他のプレイヤーにイベントの送信を仲介してもらうことができます。◆
 * 最後に実行したイベントやイベント履歴をサーバに保存し、後から参加したプレイヤーに送信することができます。◆
 * `SendCustomNetworkEventDelayedFrames`に相当するネットワークイベントの遅延送信が可能です。
@@ -19,7 +19,7 @@ SimpleNetworkは、[SimpleNetworkUdonBehavior](https://github.com/tutinoco/Simpl
 ◆の機能はまだ実装中です。
 
 ## 使い方
-シーンに`SimpleNetwork.prefab`を配置し、適当なクラスを作って`SimpleNetworkBehaviour`を継承して`SendEvent()`メソッドを実行することで、インスタンス内にいる自分を含むプレイヤーのオブジェクトに値を届けることができます。
+シーンに`SimpleNetwork.prefab`を配置し、適当なクラスを作って`SimpleNetworkBehaviour`を継承して`SendEvent()`メソッドを実行することで、インスタンス内にいるプレイヤー（自分を含む）のオブジェクトに値を届けることができます。
 イベントの受信は、サブクラスで`ReceiveEvent`メソッドをオーバーライドすることで可能となり、第一引数にイベント名が、第二引数に値が届きます。
 
 ```C#
@@ -29,18 +29,18 @@ using VRC.SDKBase;
 using VRC.Udon;
 using tutinoco;
 
-public class Test : SimpleNetworkBehaviour
+public class Test : SimpleNetworkBehaviour // 継承して
 {
     void Start()
     {
-        SimpleNetworkInit();
-        SendEvent("Talk", "こんにちは！");
+        SimpleNetworkInit(); // 初期化して
+        SendEvent("Talk", "こんにちは！"); // イベントを送ると
     }
 
     public override void ReceiveEvent(string name, string value)
     {
         if( name == "Talk" ) {
-            Debug.Log(value); // こんにちは！が全ユーザに届く
+            Debug.Log(value); // こんにちは！が全ユーザに届きます
         }
     }
 }
@@ -125,7 +125,7 @@ Monster monster = (Monster)GetObject(value); // 初期化されたSimpleNetworkB
 VRCPlayerApi player = GetSourcePlayer(value);
 
 // このイベントが送られたプレイヤー一覧を取得します
-VRCPlayerApi[] players = GetTargetPlayers(value);
+VRCPlayerApi[] players = GetSendToPlayers(value);
 
 // 送信元オブジェクトを取得します
 SimpleNetworkBehaviour behavior = GetSourceObject(value);
@@ -136,7 +136,7 @@ SimpleNetworkBehaviour[] behaviors = GetTargetObjects(value);
 // イベントに設定されたグループ名を取得します
 string group = GetGroup(value);
 
-// グループで繰り返し処理された際のインデックスを取得します。
+// グループでイテレートした際のインデックスを取得します。
 int index = GetGroupIndex(value);
 ```
 
@@ -177,7 +177,7 @@ SendEvent(SendTo.NotMaster, "Jump", 5.0f);
 SendEvent(SendTo.NotSelf, "Jump", 5.0f);
 
 // 指定のプレイヤーにイベントを送信
-SendEvent(murder.playerID, "Jump", 5.0f);
+SendEvent(player, "Jump", 5.0f);
 ```
 
 ### イベント送信の依頼
@@ -196,7 +196,7 @@ SimpleNetwork.DebugMode(true);
 ```
 
 ## 仕組み
-<p align="center"><img alt="structure" src="https://user-images.githubusercontent.com/14051445/219880088-6af5cec6-f50e-4e0b-9e54-75153155920a.png" width="60%"></p>
+<p align="center"><img alt="structure" src="https://user-images.githubusercontent.com/14051445/220154122-edd5e748-78af-4085-b28f-79192161deab.png" width="60%"></p>
 
 80人までの同時接続が可能な`SimpleNetworkProxy`（以下、プロキシ）が`SimpleNetwork.Prefab`に用意されており、プロキシには`string`型の`UdonSynced`同期変数が用意されています。
 
@@ -204,33 +204,31 @@ SimpleNetwork.DebugMode(true);
 
 `SimpleNetwork`は、シーンに存在する全ての`SimpleNetworkBehavior`から発行されたイベントを収集しており、`SimpleNetworkBehavior`を継承したオブジェクトで`SendEvent()`メソッドが実行されると、イベント情報が`SimpleNetwork`に送信され、収集した複数のイベントの名前や値などの情報は1フレーム毎にひとつのコードにシリアライズされます。
 
-作成されたコードは、専用プロキシの同期変数に投げられ、これにより全てのプレイヤーにコードが同期されます。
-
-`SimpleNetwork`は全てのプロキシを所持しており、全てのプレイヤーが発行したイベントをリアルタイムに受信します。
+作成されたコードは、専用プロキシの同期変数に投げられ、これにより全てのプレイヤーにコードが同期され、同期されたコードは`SimpleNetwork`がリアルタイムに受信します。
 
 プロキシは、`OnValueChanged`によってコードの受信を検知しており、`SimpleNetwork`は、受け取ったコードをデシリアライズして、イベントに分割します。このとき、イベントのターゲットに含まれないプレイヤーは、受信したイベントを無視するよう設定されています。
 
 実行が許されたイベントは、適切なオブジェクトの`ReceiveEvent()`メソッドを呼び出し、これにより、引数付きのネットワークイベントの発行が実現されています。
 
-また、プロキシの`UdonBehaviourSyncMode`は`Manual`に設定されており、同期変数は高速に同期されるので、`SimpleNetwork`の`SendEvent()`は、`SendCustomNetworkEvent()`よりも高速な通信を実現しています。
+また、プロキシの`UdonBehaviourSyncMode`は`Manual`に設定されており、同期変数は高速に同期されるので、`SimpleNetwork`の`SendEvent()`は、`SendCustomNetworkEvent()`よりも高速に送受信できます。
 
 ### コード
 以下の仕様は、プロキシが通信するコードについて説明しています。
 `SimpleNetwork`でどのようなデータが通信されるか知りたい場合は、以下をご覧ください。
 
-![Code](https://user-images.githubusercontent.com/14051445/220130440-ad8d3595-aa9f-4c6c-ad4b-e83cd9703a41.png)
+![Code](https://user-images.githubusercontent.com/14051445/220154173-055f55a5-02b3-4b27-a6d9-060c5b81404c.png)
 RSは「レコード区切り文字」と呼ばれる特殊な文字で、可変長のイベント名と値を分割するために使用されます。一方、GSは「グループ区切り文字」と呼ばれる特殊な文字で、複数のイベントが同時に送信される場合に、それぞれのイベントを区切るために使用されています。
 
 以下が各項目の詳細です。
 * `SyncChar`：同期用文字。コマンドが同じ場合に`UdonSynced`の同期が省略されてしまうことを防ぐために使用されます。ASCIIコードの`!`から`~`までの94文字が、コードの先頭だけに順番に挿入されます。
-* `Source`：送信元オブジェクトID。送信元オブジェクトの管理インデックスを94進数に変換した2桁の94進数です。設定によって桁数を変更でき、デフォルトでは最大8742個のオブジェクトを管理できます。
+* `Source`：送信元オブジェクトID。送信元オブジェクトの管理インデックスを2桁の94進数に変換したものです。設定によって桁数を変更でき、デフォルトでは最大8742個のオブジェクトを管理できます。
 * `SendTo`：送信先プレイヤーID。1桁の94進数で、`!`が宛先無し、`"`から`q`の80文字が`playerId`による宛先指定となります。`r`からは、`All` `Owner` `Master` `Self` `NotOwner` `NotMaster` `NotSelf`による送信先の条件指定が可能です。
 * `Target`：送信先オブジェクトID。先頭が`~`の場合、2桁目の文字がグループIDとして使用されます。デフォルトの最大オブジェクト管理数が8836個ではなく8742個なのは、これによるものです。
 * `Event Name`：イベント名です。
 * `Value`：イベントに送信された値です。`GetFloat()`などのメソッドを使用して値を受け取ることができます。
 
-#### 数の圧縮
-SimpleNetworkは、通信するコードの文字数を減らすため、94進数とsignedな93進数を利用してコードを圧縮しています。
+### コードの圧縮
+SimpleNetworkは、通信するコード量を減らすため、94進数とsignedな93進数を利用してコードを圧縮しています。
 コードに含まれるunsignedな整数は、`!`から`~`までのASCIIコード文字を利用して94進数に変換されます。
 ```
 ! " # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ \ ] ^ _ ` a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~
