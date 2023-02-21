e92165d8fdd4f05c5a10fd0696cf279b77228341
tutinoco <m@tutino.co>
2023-02-21 21:36:34 +0900

    不要になった複数のSetEvent()メソッドを削除し、SendEventがlong型に対応
    SendToにMeを追加しました。これはSelfに似ていますが異なるもので、これから実装するイベント送信依頼RequestEvent()利用時に依頼元を表すのに使う予定です。
    SimpleNetworkにToInt()メソッドを作成しました。
    この影響によりSimpleNetworkに存在していたSetEvent()のオーバーライドメソッドが不要になったため削除しました。
    またSimpleNetworkBehaviorにも定義していたSetEvent()が不要になり、削除しました。
    SimpleNetworkConverterでなぜか剰余演算子（%）を使うとコンパイルエラーになったのでMod()メソッドを作成しました。
    ToBase94()メソッドのオーバーライドメソッドを作成しlongに対応しました。
    94進数に変換したlongを元に戻すBase94ToLong()メソッドを作成しました。
    SimpleNetworkBehaviorで利用していなかったOnSimpleNetworkReady()バーチャルメソッドを削除しました。
    SimpleNetworkBehaviorのSendEvent()メソッドでlong型に対応しました。
    SimpleNetworkBehaviorのSendEvent()のInt型メソッドで値を94進数に変換するようにしました。
    SimpleNetworkBehaviorのSendEvent()のObject型メソッドでIDを94進数に変換するようにしました。

--- Scripts/SimpleNetworkBehaviour.cs
+++ Scripts/SimpleNetworkBehaviour.cs
@@ -10,6 +10,7 @@ namespace tutinoco
         public uint _id;
         private SimpleNetwork sn;
 
+        private void _i() { SimpleNetworkInit(); }
         public void SimpleNetworkInit()
         {
             if( sn != null ) return;
@@ -17,66 +18,60 @@ namespace tutinoco
             sn.SetBehaviour(this);
         }
 
-        public virtual void OnSimpleNetworkReady() { }
-
         public virtual void ReceiveEvent(string name, string value) { }
 
-        private void SetEvent(SendTo sendto, string name, string value, int delay )
-        {
-            if( sn == null ) SimpleNetworkInit();
-            sn.SetEvent(this, sendto, name, value, delay);
-        }
-
-        private void SetEvent(VRCPlayerApi sendto, string name, string value, int delay )
-        {
-            if( sn == null ) SimpleNetworkInit();
-            sn.SetEvent(this, sendto, name, value, delay);
-        }
-
         // string
-        public void ExecEvent(string name, string value="", int delay=0) { SetEvent(SendTo.Self, name, value, delay); }
-        public void SendEvent(string name, string value="", int delay=0) { SetEvent(SendTo.All, name, value, delay); }
-        public void SendEvent(SendTo sendto, string name, string value="", int delay=0) { SetEvent(sendto, name, value, delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, string value="", int delay=0) { SetEvent(sendto, name, value, delay); }
+        public void ExecEvent(string name, string value="", int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, value, delay); }
+        public void SendEvent(string name, string value="", int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, value, delay); }
+        public void SendEvent(SendTo sendto, string name, string value="", int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, value, delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, string value="", int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, value, delay); }
 
         // bool
-        public void ExecEvent(string name, bool value, int delay=0) { SetEvent(SendTo.Self, name, _Bool(value), delay); }
-        public void SendEvent(string name, bool value, int delay=0) { SetEvent(SendTo.All, name, _Bool(value), delay); }
-        public void SendEvent(SendTo sendto, string name, bool value, int delay=0) { SetEvent(sendto, name, _Bool(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, bool value, int delay=0) { SetEvent(sendto, name, _Bool(value), delay); }
+        public void ExecEvent(string name, bool value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Bool(value), delay); }
+        public void SendEvent(string name, bool value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Bool(value), delay); }
+        public void SendEvent(SendTo sendto, string name, bool value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Bool(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, bool value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Bool(value), delay); }
         public bool GetBool( string v ) { return v!="0"; }
         private string _Bool( bool v ) { return v?"1":"0"; }
 
         // int
-        public void ExecEvent(string name, int value, int delay=0) { SetEvent(SendTo.Self, name, _Int(value), delay); }
-        public void SendEvent(string name, int value, int delay=0) { SetEvent(SendTo.All, name, _Int(value), delay); }
-        public void SendEvent(SendTo sendto, string name, int value, int delay=0) { SetEvent(sendto, name, _Int(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, int value, int delay=0) { SetEvent(sendto, name, _Int(value), delay); }
-        public int GetInt( string v ) { return int.Parse(v); }
-        private string _Int( int v ) { return ""+v; }
+        public void ExecEvent(string name, int value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Int(value), delay); }
+        public void SendEvent(string name, int value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Int(value), delay); }
+        public void SendEvent(SendTo sendto, string name, int value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Int(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, int value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Int(value), delay); }
+        public int GetInt( string v ) { return SimpleNetworkConverter.Base94ToInt(v); }
+        private string _Int( int v ) { return SimpleNetworkConverter.ToBase94(v); }
+
+        // long
+        public void ExecEvent(string name, long value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Long(value), delay); }
+        public void SendEvent(string name, long value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Long(value), delay); }
+        public void SendEvent(SendTo sendto, string name, long value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Long(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, long value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Long(value), delay); }
+        public long GetLong( string v ) { return SimpleNetworkConverter.Base94ToLong(v); }
+        private string _Long( long v ) { return SimpleNetworkConverter.ToBase94(v); }
 
         // float
-        public void ExecEvent(string name, float value, int delay=0) { SetEvent(SendTo.Self, name, _Float(value), delay); }
-        public void SendEvent(string name, float value, int delay=0) { SetEvent(SendTo.All, name, _Float(value), delay); }
-        public void SendEvent(SendTo sendto, string name, float value, int delay=0) { SetEvent(sendto, name, _Float(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, float value, int delay=0) { SetEvent(sendto, name, _Float(value), delay); }
+        public void ExecEvent(string name, float value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Float(value), delay); }
+        public void SendEvent(string name, float value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Float(value), delay); }
+        public void SendEvent(SendTo sendto, string name, float value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Float(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, float value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Float(value), delay); }
         public float GetFloat( string v ) { return float.Parse(v); }
         private string _Float( float v ) { return ""+v; }
 
         // Vector3
-        public void ExecEvent(string name, Vector3 value, int delay=0) { SetEvent(SendTo.Self, name, _Vector3(value), delay); }
-        public void SendEvent(string name, Vector3 value, int delay=0) { SetEvent(SendTo.All, name, _Vector3(value), delay); }
-        public void SendEvent(SendTo sendto, string name, Vector3 value, int delay=0) { SetEvent(sendto, name, _Vector3(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, Vector3 value, int delay=0) { SetEvent(sendto, name, _Vector3(value), delay); }
+        public void ExecEvent(string name, Vector3 value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Vector3(value), delay); }
+        public void SendEvent(string name, Vector3 value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Vector3(value), delay); }
+        public void SendEvent(SendTo sendto, string name, Vector3 value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Vector3(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, Vector3 value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Vector3(value), delay); }
         public Vector3 GetVector3( string v ) { string[] d=v.Split(','); return new Vector3(float.Parse(d[0]), float.Parse(d[1]), float.Parse(d[2])); }
         private string _Vector3( Vector3 v ) { return v.x+","+v.y+","+v.z; }
 
         // Object
-        public void ExecEvent(string name, SimpleNetworkBehaviour value, int delay=0) { SetEvent(SendTo.Self, name, _Object(value), delay); }
-        public void SendEvent(string name, SimpleNetworkBehaviour value, int delay=0) { SetEvent(SendTo.All, name, _Object(value), delay); }
-        public void SendEvent(SendTo sendto, string name, SimpleNetworkBehaviour value, int delay=0) { SetEvent(sendto, name, _Object(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, SimpleNetworkBehaviour value, int delay=0) { SetEvent(sendto, name, _Object(value), delay); }
-        public SimpleNetworkBehaviour GetObject( string v ) { return sn.GetBehaviour(int.Parse(v)); }
-        private string _Object( SimpleNetworkBehaviour v ) { return ""+v._id; }
+        public void ExecEvent(string name, SimpleNetworkBehaviour value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Object(value), delay); }
+        public void SendEvent(string name, SimpleNetworkBehaviour value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Object(value), delay); }
+        public void SendEvent(SendTo sendto, string name, SimpleNetworkBehaviour value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Object(value), delay); }
+        public void SendEvent(VRCPlayerApi sendto, string name, SimpleNetworkBehaviour value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Object(value), delay); }
+        public SimpleNetworkBehaviour GetObject( string v ) { return sn.GetBehaviour(SimpleNetworkConverter.Base94ToInt(v)); }
+        private string _Object( SimpleNetworkBehaviour v ) { return SimpleNetworkConverter.ToBase94(v._id); }
     }
 }
