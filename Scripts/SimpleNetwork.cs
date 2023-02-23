b5d8a5359cba51924c3be23874f3bdad497d2004
tutinoco <m@tutino.co>
2023-02-23 18:52:20 +0900

    イベントの送信方法を根本から見直し、コードに変換せずに対応する型の配列に追加して送信する方法に変更
    コード化の廃止により、SimpleNetworkクラスで利用していた特殊文字rsとgsを削除しました。
    コードの見直しの結果isInitとisReadyプロパティが不要になったので削除しました。
    isReady廃止の代わりに、myProxyがnullでなければUpdate()が実行されるようになっています。
    myProxyの導入により不要になったGetProxy()メソッドを廃止しました。
    syncCountプロパティを廃止し、SimpleNetworkProxyのcountに移しました。
    コードが廃止されたため、94進数管理桁数を制御するbehaviorDigitの概念も無くなりました。
    rSendTo, rSources, rNames, rValues, rDelaysが廃止されevObjsでひとまとまりで管理するようにしました。
    この変更に伴い、SimpleNetworkBehaviorやProxyにも直接evObjを渡す形式に大きく変更しています。
    また各配列に値を代入するのに使っていたInsertElement()メソッドが不要になったので廃止しました。
    変数の初期化以外必要なくなったため、Initialize()メソッドを廃止してクラス変数の宣言と同時に初期化するようにしました。
    コード廃止のため、JoinRecord()とSplitRecord()メソッドを廃止しました。
    コードを同期すれば良いだけSimpleNetworkProxyで、通信に対応する型の配列を用意しました。
    また、EventLength(), GetEvent(), SetEvent(), SyncEvents()のメソッドを利用してイベントの内容を取得したり同期したりするようにしました。
    Public定義していたProxyのIdをGetNum()メソッドを使って取得するように変更しました。
    イベントの受信を判定するのにOnValueChangedを利用していたのをOnDeserialization()を利用する方式に切り替えました。
    一度送信したイベントを繰り返し送信してしまうことを防ぐためにProxyのSyncEvents()にisWaitingをつけるようにしました。
    文字列変換の必要が失われたため、SimpleNetworkConverterクラスを削除しました。
    SimpleNetworkBehaviorでイベントを受信する方法に、ダイレクトレシーブ（キャスト済の型が直接メソッドに届く）を使えるようにしました。
    イベントの遅延実行のカウント方法をDelay値の少数部を利用してカウントする方法に変更しました。
    SimpleNetworkBehaviorで対応していたlongとobjectを一時的に廃止しました。

--- Scripts/SimpleNetwork.cs
+++ Scripts/SimpleNetwork.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using UdonSharp;
+﻿using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+
+using Object = System.Object;
 
 namespace tutinoco
 {
@@ -18,31 +19,21 @@ namespace tutinoco
         Me,
     }
 
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
     public class SimpleNetwork : UdonSharpBehaviour
     {
         private bool isDebugMode;
 
-        private SimpleNetworkBehaviour[] behaviours;
-        private SimpleNetworkProxy[] proxys;
-        private int[] refer;
-        private char gs = (char)0x1D;
-        private char rs = (char)0x1E;
-        private bool isInit;
-        private bool isReady;
-        private int syncCount;
-        private int behaviorDigit = 2;
-
-        private int[] rSendTo;
-        private SimpleNetworkBehaviour[] rSources;
-        private string[] rNames;
-        private string[] rValues;
-        private int[] rDelays;
+        private Object[][] evObjs = new Object[0][];
+        private SimpleNetworkBehaviour[] behaviours = new SimpleNetworkBehaviour[0];
+        [SerializeField] private SimpleNetworkProxy[] proxys;
+        private SimpleNetworkProxy myProxy;
+        private int[] refer = new int[0];
 
         public static SimpleNetwork GetInstance()
         {
             GameObject g = GameObject.Find("SimpleNetwork");
             SimpleNetwork sn = g.GetComponent<SimpleNetwork>();
-            sn.Initialize();
             return sn;
         }
 
@@ -52,83 +43,27 @@ namespace tutinoco
             sn.isDebugMode = flg;
         }
 
-        public static T[] InsertElement<T>(T[] ary, int idx, T elm)
+        void Update()
         {
-            T[] tmp = new T[ary.Length + 1];
-            int j = 0;
-            for (int i = 0; i < tmp.Length; i++) {
-                if (i == idx) tmp[i] = elm;
-                else { tmp[i]=ary[j]; j++; }
-            }
-            return tmp;
-        }
+            if( myProxy==null ) return;
 
-        private void Initialize()
-        {
-            if( isInit ) return;
-            isInit = true;
-
-            behaviours = new SimpleNetworkBehaviour[0];
-            refer = new int[0];
-
-            rSendTo = new int[0];
-            rDelays = new int[0];
-            rSources = new SimpleNetworkBehaviour[0];
-            rNames = new string[0];
-            rValues = new string[0];
-
-            int len = gameObject.transform.childCount;
-            proxys = new SimpleNetworkProxy[len+1];
-            for(int i=1; i<len; i++) {
-                Transform t = transform.GetChild(i);
-                proxys[i] = t.gameObject.GetComponent<SimpleNetworkProxy>();
-                proxys[i].id = i;
-                proxys[i].sn = this;
-            }
-        }
-
-        private void Update()
-        {
-            if( !isReady ) return;
-
-            string code = "";
-            for(var i=0; i<rDelays.Length; i++) {
-                if( rDelays[i] == 0 ) {
-                    if( rSendTo[i] == (int)SendTo.Self ) rSources[i].ReceiveEvent(rNames[i], rValues[i]);
-                    else code += (code==""?"":gs.ToString()) + JoinRecord(rSources[i], rSendTo[i], rSources[i]/*とりあえず*/, rNames[i], rValues[i]);
+            int delayNum = 0;
+            foreach( var evObj in evObjs ) {
+                double delay = (double)evObj[4];
+                int counter = (int)((delay-(int)delay) * 1000000);
+                if( (int)delay == counter ) { myProxy.SetEvent(evObj); }
+                else {
+                    evObj[4] = delay+0.000001;
+                    evObjs[delayNum] = evObj;
+                    delayNum++;
                 }
-                if( rDelays[i] >= 0) rDelays[i]--;
             }
 
-            if( code != "" ) {
-                string syncchar = SimpleNetworkConverter.Padding(SimpleNetworkConverter.ToBase94(syncCount++), 1);
-                code = syncchar + code;
-                if( isDebugMode ) Debug.Log("SimpleNetwork Send Code: "+code.Replace(""+rs,":").Replace(""+gs,","));
-                GetProxy(Networking.LocalPlayer).Sync(code);
-                code="";
-            }
-        }
+            Object[][] tmp = new Object[delayNum][];
+            for(int i=0; i<delayNum; i++) tmp[i]=evObjs[i];
+            evObjs = tmp;
 
-        public string JoinRecord( SimpleNetworkBehaviour source, int player, SimpleNetworkBehaviour target, string name, string value )
-        {
-            string s = SimpleNetworkConverter.Padding(SimpleNetworkConverter.ToBase94((int)source._id), behaviorDigit);
-            string p = SimpleNetworkConverter.Padding(SimpleNetworkConverter.ToBase94(player), 1);
-            string t = SimpleNetworkConverter.Padding(SimpleNetworkConverter.ToBase94((int)target._id), behaviorDigit);
-            return s + p + t + name + rs + value;
-        }
-
-        public string[] SplitRecord( string record )
-        {
-            string[] ary = new string[5];
-
-            ary[0] = record.Substring(0, behaviorDigit);
-            ary[1] = ""+record[behaviorDigit];
-            ary[2] = record.Substring(1+behaviorDigit, 2);
-            string[] namevalue = record.Substring(1+behaviorDigit*2).Split(rs);
-            ary[3] = namevalue[0];
-            ary[4] = namevalue[1];
-
-            return ary;
+            myProxy.SyncEvents();
         }
 
         public SimpleNetworkBehaviour GetBehaviour( int id )
@@ -138,37 +73,27 @@ namespace tutinoco
 
         public void SetBehaviour(SimpleNetworkBehaviour behaviour)
         {
-            int index = behaviours.Length;
+            int idx = behaviours.Length;
             string x = Networking.GetUniqueName(behaviour.gameObject);
             for (int i=0; i<behaviours.Length; i++) {
                 string y = Networking.GetUniqueName(behaviours[i].gameObject);
-                if (y.CompareTo(x) > 0) { index=i; break; }
+                if (y.CompareTo(x) > 0) { idx=i; break; }
             }
             var temp = new SimpleNetworkBehaviour[behaviours.Length+1];
-            for (int i=0; i<index; i++) temp[i] = behaviours[i];
-            behaviour._id = (uint)index;
-            temp[index] = behaviour;
-            for (int i = index+1; i<temp.Length; i++) { temp[i]=behaviours[i-1]; temp[i]._id++; }
+            for (int i=0; i<idx; i++) temp[i] = behaviours[i];
+            behaviour._id = idx;
+            temp[idx] = behaviour;
+            for (int i = idx+1; i<temp.Length; i++) { temp[i]=behaviours[i-1]; temp[i]._id++; }
             behaviours = temp;
         }
 
-        public void SetEvent( SimpleNetworkBehaviour source, int sendto, string name, string value, int delay )
+
+        public void SetEvent( Object[] evObj )
         {
-            int idx = rDelays.Length;
-            for (int i=0; i<rDelays.Length; i++) if(delay>rDelays[i]){idx=i;break;}
-            if( rDelays.Length != idx && rDelays[idx] == -1 ) {
-                rSendTo[idx] = sendto;
-                rDelays[idx] = delay;
-                rSources[idx] = source;
-                rNames[idx] = name;
-                rValues[idx] = value;
-            } else {
-                rSendTo = InsertElement(rSendTo, idx, sendto);
-                rDelays = InsertElement(rDelays, idx, delay);
-                rSources = InsertElement(rSources, idx, source);
-                rNames = InsertElement(rNames, idx, name);
-                rValues = InsertElement(rValues, idx, value);
-            }
+            Object[][] tmp = new Object[evObjs.Length+1][];
+            evObjs.CopyTo(tmp, 0);
+            tmp[evObjs.Length] = evObj;
+            evObjs = tmp;
         }
 
         private bool IsProxyUsed( int i )
@@ -179,13 +104,13 @@ namespace tutinoco
 
         private void AttachProxy( VRCPlayerApi player, SimpleNetworkProxy proxy )
         {
-            int[] temp = new int[player.playerId];
+            int pid = player.playerId;
+            int[] temp = new int[pid];
             refer.CopyTo(temp, 0);
             refer = temp;
-            int i = player.playerId - 1;
-            refer[i] = proxy.id;
+            refer[pid-1] = proxy.GetNum();
 
-            if( isDebugMode ) Debug.Log("SimpleNetwork Assigned Proxy #"+proxy.id+" to "+player.displayName);
+            if( isDebugMode ) Debug.Log("SimpleNetwork Assigned Proxy #"+proxy.GetNum()+" to "+player.displayName);
         }
 
         private void DetachProxy( VRCPlayerApi player )
@@ -195,21 +120,10 @@ namespace tutinoco
             refer[i] = 0;
         }
 
-        private SimpleNetworkProxy GetProxy( VRCPlayerApi player )
-        {
-            int i = player.playerId - 1;
-            return proxys[refer[i]];
-        }
-
         public int ToInt( SendTo sendto )
         {
             if( sendto == SendTo.Me ) return Networking.LocalPlayer.playerId;
             return proxys.Length + (int)sendto;
-        }
-
-        public int ToInt( RequestTo request )
-        {
-            return proxys.Length + (int)request;
         }
 
         public int ToInt( VRCPlayerApi player )
@@ -217,17 +131,16 @@ namespace tutinoco
             return player.playerId;
         }
 
-        public void OnProxySynced( string code, SimpleNetworkProxy proxy )
+        public void OnProxySynced( SimpleNetworkProxy proxy )
         {
-            if( isDebugMode ) Debug.Log("SimpleNetwork Receive Code: "+code.Replace(""+rs,":").Replace(""+gs,","));
-            // VRCPlayerApi player = Networking.GetOwner(proxy.gameObject);
-            foreach( string record in code.Substring(1).Split(gs) ) {
-                string[] data = SplitRecord(record);
-                int source = SimpleNetworkConverter.Base94ToInt(data[0]);
-                int sendto = SimpleNetworkConverter.Base94ToInt(data[1]);
-                int target = SimpleNetworkConverter.Base94ToInt(data[2]);
-                SimpleNetworkBehaviour behaviour = behaviours[target];
+            for(int i=0; i<proxy.EventLength(); i++) {
+                Object[] evObj = proxy.GetEvent(i);
+                var source = (SimpleNetworkBehaviour)evObj[0];
+                int sendto = (int)evObj[1];
+                int delay = (int)evObj[4];
 
+                var behaviour = source; // ひとまず
+                
                 bool isReceive = false;
                 if( sendto == proxys.Length+(int)SendTo.All ) isReceive = true;
                 else if( sendto == proxys.Length+(int)SendTo.Owner && Networking.IsOwner(behaviour.gameObject) ) isReceive = true;
@@ -239,7 +152,7 @@ namespace tutinoco
                 else if( Networking.LocalPlayer.playerId == sendto ) isReceive = true;
                 if( !isReceive ) continue;
 
-                behaviour.ReceiveEvent(data[3], data[4]);
+                behaviour._ReceiveEvent(evObj);
             }
         }
 
@@ -247,23 +160,20 @@ namespace tutinoco
         {
             VRCPlayerApi player = Networking.GetOwner(proxy.gameObject);
             AttachProxy(player, proxy);
-            if( Networking.LocalPlayer == player ) isReady = true;
+            if( Networking.LocalPlayer == player ) myProxy = proxy;
         }
 
         public override void OnPlayerJoined(VRCPlayerApi player)
         {
             if( !Networking.IsMaster ) return;
 
-            int pid = player.playerId;
-            for(var i=1; i<proxys.Length+1; i++) {
+            for(var i=0; i<proxys.Length; i++) {
                 SimpleNetworkProxy proxy = proxys[i];
                 if( IsProxyUsed(i) ) continue;
                 if( Networking.IsOwner(player, proxy.gameObject) ) OnProxyOwnershipTransferred(proxy);
                 else Networking.SetOwner(player, proxy.gameObject);
                 break;
             }
-
-            RequestSerialization();
         }
 
         public override void OnPlayerLeft(VRCPlayerApi player)
--- Scripts/SimpleNetworkBehaviour.cs
+++ Scripts/SimpleNetworkBehaviour.cs
@@ -1,16 +1,20 @@
-﻿using UdonSharp;
+﻿using System;
+using UdonSharp;
 using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
+
+using Object = System.Object;
 
 namespace tutinoco
 {
     public class SimpleNetworkBehaviour : UdonSharpBehaviour
     {
-        public uint _id;
+        public int _id;
         private SimpleNetwork sn;
+        private Object[] obj;
 
-        private void _i() { SimpleNetworkInit(); }
+        private void I() { SimpleNetworkInit(); }
         public void SimpleNetworkInit()
         {
             if( sn != null ) return;
@@ -18,60 +22,80 @@ namespace tutinoco
             sn.SetBehaviour(this);
         }
 
-        public virtual void ReceiveEvent(string name, string value) { }
+        public void _ReceiveEvent(Object[] evObj) {
+            obj = evObj;
+            string name = (string)evObj[2];
+            var value = evObj[3];
+            ReceiveEvent(name);
 
-        // string
-        public void ExecEvent(string name, string value="", int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, value, delay); }
-        public void SendEvent(string name, string value="", int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, value, delay); }
-        public void SendEvent(SendTo sendto, string name, string value="", int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, value, delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, string value="", int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, value, delay); }
+            Type t = value.GetType();
+            if( t == typeof(bool) ) ReceiveEvent(name, (bool)value);
+            else if( t == typeof(char) ) ReceiveEvent(name, (char)value);
+            else if( t == typeof(byte) ) ReceiveEvent(name, (byte)value);
+            else if( t == typeof(sbyte) ) ReceiveEvent(name, (sbyte)value);
+            else if( t == typeof(short) ) ReceiveEvent(name, (short)value);
+            else if( t == typeof(ushort) ) ReceiveEvent(name, (ushort)value);
+            else if( t == typeof(int) ) ReceiveEvent(name, (int)value);
+            else if( t == typeof(uint) ) ReceiveEvent(name, (uint)value);
+            else if( t == typeof(long) ) ReceiveEvent(name, (long)value);
+            else if( t == typeof(ulong) ) ReceiveEvent(name, (ulong)value);
+            else if( t == typeof(float) )  ReceiveEvent(name, (float)value);
+            else if( t == typeof(double) )  ReceiveEvent(name, (double)value);
+            else if( t == typeof(Vector2) ) ReceiveEvent(name, (Vector2)value);
+            else if( t == typeof(Vector3) ) ReceiveEvent(name, (Vector3)value);
+            else if( t == typeof(Vector4) ) ReceiveEvent(name, (Vector4)value);
+            else if( t == typeof(Quaternion) ) ReceiveEvent(name, (Quaternion)value);
+            else if( t == typeof(string) ) ReceiveEvent(name, (string)value);
+            else if( t == typeof(VRCUrl) ) ReceiveEvent(name, (VRCUrl)value);
+            else if( t == typeof(Color) ) ReceiveEvent(name, (Color)value);
+            else if( t == typeof(Color32) ) ReceiveEvent(name, (Color32)value);
+        }
+
+        public virtual void ReceiveEvent(string name) { }
+        public virtual void ReceiveEvent(string name, bool value) { }
+        public virtual void ReceiveEvent(string name, char value) { }
+        public virtual void ReceiveEvent(string name, byte value) { }
+        public virtual void ReceiveEvent(string name, sbyte value) { }
+        public virtual void ReceiveEvent(string name, short value) { }
+        public virtual void ReceiveEvent(string name, ushort value) { }
+        public virtual void ReceiveEvent(string name, int value) { }
+        public virtual void ReceiveEvent(string name, uint value) { }
+        public virtual void ReceiveEvent(string name, long value) { }
+        public virtual void ReceiveEvent(string name, ulong value) { }
+        public virtual void ReceiveEvent(string name, float value) { }
+        public virtual void ReceiveEvent(string name, double value) { }
+        public virtual void ReceiveEvent(string name, Vector2 value) { }
+        public virtual void ReceiveEvent(string name, Vector3 value) { }
+        public virtual void ReceiveEvent(string name, Vector4 value) { }
+        public virtual void ReceiveEvent(string name, Quaternion value) { }
+        public virtual void ReceiveEvent(string name, string value) { }
+        public virtual void ReceiveEvent(string name, VRCUrl value) { }
+        public virtual void ReceiveEvent(string name, Color value) { }
+        public virtual void ReceiveEvent(string name, Color32 value) { }
+
+        // meta
+        public int GetDelay() { return (int)obj[4]; }
+        public SimpleNetworkBehaviour GetSource() { return (SimpleNetworkBehaviour)obj[0]; }
 
         // bool
-        public void ExecEvent(string name, bool value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Bool(value), delay); }
-        public void SendEvent(string name, bool value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Bool(value), delay); }
-        public void SendEvent(SendTo sendto, string name, bool value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Bool(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, bool value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Bool(value), delay); }
-        public bool GetBool( string v ) { return v!="0"; }
-        private string _Bool( bool v ) { return v?"1":"0"; }
-
-        // int
-        public void ExecEvent(string name, int value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Int(value), delay); }
-        public void SendEvent(string name, int value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Int(value), delay); }
-        public void SendEvent(SendTo sendto, string name, int value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Int(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, int value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Int(value), delay); }
-        public int GetInt( string v ) { return SimpleNetworkConverter.Base94ToInt(v); }
-        private string _Int( int v ) { return SimpleNetworkConverter.ToBase94(v); }
-
-        // long
-        public void ExecEvent(string name, long value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Long(value), delay); }
-        public void SendEvent(string name, long value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Long(value), delay); }
-        public void SendEvent(SendTo sendto, string name, long value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Long(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, long value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Long(value), delay); }
-        public long GetLong( string v ) { return SimpleNetworkConverter.Base94ToLong(v); }
-        private string _Long( long v ) { return SimpleNetworkConverter.ToBase94(v); }
+        public void ExecEvent(string name, bool value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(SendTo.Self), name, value, delay}); }
+        public void SendEvent(string name, bool value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(SendTo.All), name, value, delay}); }
+        public void SendEvent(SendTo sendto, string name, bool value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(sendto), name, value, delay}); }
+        public void SendEvent(VRCPlayerApi sendto, string name, bool value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(sendto), name, value, delay}); }
+        public bool GetBool() { return (bool)obj[3]; }
 
         // float
-        public void ExecEvent(string name, float value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Float(value), delay); }
-        public void SendEvent(string name, float value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Float(value), delay); }
-        public void SendEvent(SendTo sendto, string name, float value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Float(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, float value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Float(value), delay); }
-        public float GetFloat( string v ) { return float.Parse(v); }
-        private string _Float( float v ) { return ""+v; }
+        public void ExecEvent(string name, float value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(SendTo.Self), name, value, delay}); }
+        public void SendEvent(string name, float value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(SendTo.All), name, value, delay}); }
+        public void SendEvent(SendTo sendto, string name, float value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(sendto), name, value, delay}); }
+        public void SendEvent(VRCPlayerApi sendto, string name, float value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(sendto), name, value, delay}); }
+        public float GetFloat() { return (float)obj[3]; }
 
         // Vector3
-        public void ExecEvent(string name, Vector3 value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Vector3(value), delay); }
-        public void SendEvent(string name, Vector3 value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Vector3(value), delay); }
-        public void SendEvent(SendTo sendto, string name, Vector3 value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Vector3(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, Vector3 value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Vector3(value), delay); }
-        public Vector3 GetVector3( string v ) { string[] d=v.Split(','); return new Vector3(float.Parse(d[0]), float.Parse(d[1]), float.Parse(d[2])); }
-        private string _Vector3( Vector3 v ) { return v.x+","+v.y+","+v.z; }
-
-        // Object
-        public void ExecEvent(string name, SimpleNetworkBehaviour value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.Self), name, _Object(value), delay); }
-        public void SendEvent(string name, SimpleNetworkBehaviour value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(SendTo.All), name, _Object(value), delay); }
-        public void SendEvent(SendTo sendto, string name, SimpleNetworkBehaviour value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Object(value), delay); }
-        public void SendEvent(VRCPlayerApi sendto, string name, SimpleNetworkBehaviour value, int delay=0) { _i(); sn.SetEvent(this, sn.ToInt(sendto), name, _Object(value), delay); }
-        public SimpleNetworkBehaviour GetObject( string v ) { return sn.GetBehaviour(SimpleNetworkConverter.Base94ToInt(v)); }
-        private string _Object( SimpleNetworkBehaviour v ) { return SimpleNetworkConverter.ToBase94(v._id); }
+        public void ExecEvent(string name, Vector3 value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(SendTo.Self), name, value, delay}); }
+        public void SendEvent(string name, Vector3 value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(SendTo.All), name, value, delay}); }
+        public void SendEvent(SendTo sendto, string name, Vector3 value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(sendto), name, value, delay}); }
+        public void SendEvent(VRCPlayerApi sendto, string name, Vector3 value, int delay=0) { I(); sn.SetEvent(new Object[]{this, sn.ToInt(sendto), name, value, delay}); }
+        public Vector3 GetVector3() { return (Vector3)obj[3]; }
     }
 }
